0eb82079f7bc112785397e6d7f636589a5caaa86
Anthony Massicci <anthony.massicci@outlook.com>
2018-05-02 21:34:45 -0400

    added test for route mounting

---
+++ test/routes/mountTest.js
@@ -0,0 +1,30 @@
+const mockery = require('mockery');
+const sinon = require('sinon');
+const sinonTest = require('sinon-test')(sinon);
+const RouteConstants = require('../../routes/RouteConstants');
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+
+chai.use(sinonChai);
+
+describe('mountRoutes', () => {
+
+    before(() => {
+        mockery.enable();
+    });
+
+    it('should mount all routes specified in RouteConstants', sinonTest(function() {
+        const useSpy = this.spy();
+        const RouterStub = this.stub().returns({use: useSpy});
+
+        mockery.registerMock('express', {Router: RouterStub});
+        const mountRoutes = require('../../routes');
+        
+        mountRoutes({});
+
+        for(route in RouteConstants)
+            useSpy.should.have.been.calledWith(RouteConstants[route]);
+    }));
+
+    after(() => mockery.disable());
+});
--- test/test.js
+++ test/test.js
@@ -1,1 +1,2 @@
 require('./db/MongoProviderTest');
+require('./routes/mountTest');
