e199b696565331789b12662cdd66228f70814f8f
Anthony Massicci <anthony.massicci@outlook.com>
2018-05-02 20:46:13 -0400

    rewrote the tests, made changes to MongoDB provider

---
+++ db/MongoProvider.js
@@ -0,0 +1,27 @@
+const { MongoClient } = require('mongodb');
+const { promisify } = require('util');
+
+const makeDbServices = db => coll => ({
+    query: function(queryParam) {
+        return promisify(db.collection(this.coll)
+            .find(queryParam).toArray);
+    },
+    remove: function(queryParam) {
+        return db.collection(this.coll).deleteOne(queryParam);
+    },
+    modify: function(queryParam, newValue) {
+
+    },
+    db,
+    coll,
+});
+
+const DBNAME = process.env.DBNAME || "lpg";
+
+module.exports = function(uri) {
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(uri)
+            .then(conn => resolve(makeDbServices(conn.db(DBNAME))))
+            .catch((e) => reject(e));
+    });
+};
--- test/db/MongoProviderTest.js
+++ test/db/MongoProviderTest.js
@@ -1,59 +1,122 @@
 const sinon = require('sinon');
+const sinonTest = require('sinon-test')(sinon);
 const mockery = require('mockery');
-const expect = require('chai').expect;
+const should = require('chai').should();
+const assert = require('assert');
+const { MongoClient } = require('mongodb');
+describe('MongoProvider', () => {
+    var mongoProvider;
 
-describe('MongoProvider', function() {
-    var dbConnection, mongoConnectMock;
-
-    before(function() {
+    before(() => {
         mockery.enable();
-        mongoConnectMock = sinon.stub();
-        mockery.registerMock('mongodb', {
-            MongoClient: {
-                connect: mongoConnectMock,
-            },
+        mockery.registerMock('mongodb', {MongoClient});
+        mongoProvider = require('../../db/MongoProvider');
+    });
+
+    it('should return a db services object upon successful connection', sinonTest(function(done) {
+        let MongoClientMock = this.mock(MongoClient);
+        MongoClientMock.expects("connect")
+            .once()
+            .returns(Promise.resolve({db: () => {}}))
+        
+        mongoProvider('mongodb://localhost:27017/').then(dbServices => {
+            let keys = ['query', 'modify', 'remove'];
+            dbServices.should.be.a('function');
+            dbServices('testCollection').should.have.all.keys([...keys, 'db', 'coll']);
+            keys.forEach(key => dbServices('testCollection')[key].should.be.a('function'));
+            done();
+        }).catch(e => done(e));
+
+    }));
+
+    it('should try to connect to db specified by uri', sinonTest(function(done) {
+        const TEST_URI = "mongodb://localhost:27017/";
+        let MongoClientMock = this.mock(MongoClient);
+        MongoClientMock.expects("connect")
+            .once()
+            .withArgs(TEST_URI)
+            .returns(Promise.resolve({db: function() {}}));
+        
+        mongoProvider(TEST_URI).then(() => {
+            MongoClientMock.verify();
+            done();
+        }).catch(e => done(e));
+    }));
+
+    it('should return an error upon unsuccessful connection', sinonTest(function(done) {
+        let MongoClientMock = this.mock(MongoClient);
+        MongoClientMock.expects("connect")
+            .once()
+            .returns(Promise.reject(Error('test error')));
+        
+        mongoProvider('')
+            .then(() => done(Error('this should not be successful.')))
+            .catch(e => done());
+    }));
+
+    context('with a successful connection to the database', () => {
+        let buildDbSpy;
+        before(() => {
+            buildDbSpy = (...spies) => ({
+                db: () => ({
+                    collection: () => ({
+                        find: spies[0],
+                        deleteOne: spies[1],
+                    }),
+                })
+            });
         });
-        dbConnection = require('../../db/MongoProvider');
-    });
 
-    it('should return DB services on successful connection', function(done) {
-        mongoConnectMock.returns(Promise.resolve({}));
-        dbConnection('').then(dbServices => {
-            try {
-                let keys = ['get', 'remove', 'modify', 'userModels'];
-                expect(dbServices).to.not.be.null;
-                expect(dbServices).to.include.all.keys(keys.concat('conn'));
-                keys.forEach(key => expect(dbServices[key]).to.be.a('function'));
-                done();
-            } catch(e) {
-                done(e);
-            }
+        describe('#query', () => {
+            it('should query database with specified query parameter', sinonTest(function(done) {
+                const MongoClientMock = this.mock(MongoClient);
+                const findSpy = this.spy(() => ({toArray: this.stub()}));
+                const deleteSpy = this.spy();
+                const dbSpy = buildDbSpy(findSpy, deleteSpy);
+                const queryParam = {test: 'name'};
+
+                MongoClientMock.expects("connect")
+                    .once()
+                    .returns(Promise.resolve(dbSpy));
+                
+                mongoProvider('')
+                    .then(dbServices => {
+                        let coll = dbServices('testCollection');
+                        return coll.query(queryParam);
+                    })
+                    .then(() => {
+                        assert(findSpy.calledOnce);
+                        done();
+                    })
+                    .catch(done);
+            }));
+        });
+
+        describe('#remove', () => {
+            it('should delete documents based on query parameter', sinonTest(function(done) {
+                const MongoClientMock = this.mock(MongoClient);
+                const findSpy = this.spy(() => ({toArray: this.stub()}));
+                const deleteSpy = this.spy();
+                const dbSpy = buildDbSpy(findSpy, deleteSpy);
+                const queryParam = {test: 'name'};
+
+                MongoClientMock.expects("connect")
+                    .once()
+                    .returns(Promise.resolve(dbSpy));
+                
+                mongoProvider('')
+                    .then(dbServices => {
+                        let coll = dbServices('testCollection');
+                        return coll.remove(queryParam);
+                    })
+                    .then(() => {
+                        assert(deleteSpy.calledOnce);
+                        done();
+                    })
+                    .catch(done);
+            }));
         });
     });
 
-    it('should return an error upon unsuccessful connection', function(done) {
-        mongoConnectMock.returns(Promise.reject({}));
-        dbConnection('').catch(error => {
-            expect(error).to.be.an('error');
-        })
-        .catch(e => done(e));
-    });
-
-    describe('#get', function() {
-        var dbMock;
-
-        before(function() {
-            dbMock = {
-
-            };
-        });
-
-        it('should query the database with the correct id', function(done) {
-
-        });
-    });
-
-    after(function() {
-        mockery.disable();
-    });
+    after(() => mockery.disable());
 });
