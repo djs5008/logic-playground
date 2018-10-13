74bf839bc935ca8195cded422216fbe48f9683e7
Dan Schmitt <djs5008@rit.edu>
2018-10-13 02:28:34 -0400

    style enhancements, added title

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import '../css/App.css';
-import Canvas from './canvas';
 import '../lpg/core';
+import ControlBar from './control-bar';
+import Canvas from './canvas';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className='Root'>
+        <a href='' style={{ 'text-decoration': 'none' }} >
+          <h1 className='Title'>
+            Logic Playground
+          </h1>
+        </a>
         <div className='App'>
-          <Canvas/>
+          <ControlBar />
+          <Canvas />
+          <ControlBar />
         </div>
+        <caption className='Copyright'>
+          Copyright &copy; { new Date().getFullYear() } LPG
+        </caption>
       </div>
     );
   }
---
+++ src/components/control-bar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/ControlBar.css';
+
+export default class ControlBar extends React.Component {
+  
+  render() {
+    return (
+      <div className='ControlBar'>
+        
+      </div>
+    );
+  }
+
+}
